2ea7d63c8c5cdee7de85f8f105e42e6604ea166e
shubhamsj1207 <shubhamsj1207@gmail.com>
2024-08-30 12:58:44 +0530

    new api

--- validation.js
+++ validation.js
@@ -5,6 +5,7 @@ const customerAddschema = Joi.object().keys({
     lastname: Joi.string().alphanum().min(3).max(30).required(),
     mobileno: Joi.number().integer().min(10).max(10), 
 }); 
+console.log('sddsf');
 const result =  customerAddschema.validate(data); 
 return result;
 }
