14cbe04bb66ddef6e781bec687229bf20d81f640
shubhamsj1207 <shubhamsj1207@gmail.com>
2024-08-30 17:44:12 +0530

    console added

--- src/controller/customer.js
+++ src/controller/customer.js
@@ -3,6 +3,7 @@ const coustmersdata = require("../models/mens");
 
  async function savecustomer(data)  {
     console.log('in controller', data);
+    console.log('amar');
     const addingcoustmer= new coustmersdata(data);
     const insertCoust=await addingcoustmer.save();
     return insertCoust;
